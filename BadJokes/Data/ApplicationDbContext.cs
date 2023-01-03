669d823b3df7f3b9ebd7ddd52a99b08c63e0238e
Мирослав Недялков <19311050@students.bfu.bg>
2023-01-03 15:29:17 +0200

    Add project files.

---
+++ BadJokes/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BadJokes.Models;
+
+namespace BadJokes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BadJokes.Models.Joke> Joke { get; set; } = default!;
+    }
+}
